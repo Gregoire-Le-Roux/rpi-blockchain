369a9a68e53213003b04545e9c0d8b606520be4b
Gregoire-Le-Roux <gregoire.leroux.pro@gmail.com>
2022-06-21 16:05:52 +0200

    Edit wedding certificate styles and comments

--- projets/mariage/src/App.js
+++ projets/mariage/src/App.js
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 import './App.css';
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPeopleArrowsLeftRight } from "@fortawesome/free-solid-svg-icons";
+import { faPeopleArrowsLeftRight, faIdCardClip } from "@fortawesome/free-solid-svg-icons";
+
 import WeddingCertificate from './artifacts/contracts/WeddingCertificate.sol/WeddingCertificate.json';
 
+// Wedding Certificate Address get when contract is deployed
 const weddingCertificateAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
 function App() {
@@ -18,15 +24,19 @@ function App() {
     fetchWeddingCertificate();
   }, [])
 
+  // Request to access an ethereum account
   async function requestAccount() {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
   }
 
+  // Get partners from wedding certificate
   async function fetchWeddingCertificate() {
     if (typeof window.ethereum !== 'undefined') {
+      // Get provider and contract from wedding certificate address
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const contract = new ethers.Contract(weddingCertificateAddress, WeddingCertificate.abi, provider);
       try {
+        // Get partners and update front values
         const _partnerOne = await contract.getPartnerOne();
         setPartnerOne(_partnerOne);
         const _partnerTwo = await contract.getPartnerTwo();
@@ -38,14 +48,18 @@ function App() {
     }
   }
 
+  // Set wedding certificate partners 
   async function setWeddingCertificate() {
     if (!inputPartnerOne && !inputPartnerTwo) return
     if (typeof window.ethereum !== 'undefined') {
       await requestAccount();
+      // Get provider, signer and contract from wedding certificate address
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
       const contract = new ethers.Contract(weddingCertificateAddress, WeddingCertificate.abi, signer);
+      // Set Partners Contract Method
       const transaction = await contract.setPartners(inputPartnerOne, inputPartnerTwo);
+      // Reset Input Partners
       setInputPartnerOne('');
       setInputPartnerTwo('');
       await transaction.wait();
@@ -55,17 +69,25 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <h2>Add your wedding certificat !</h2>
-      </div>
-      <div>
-        <input value={inputPartnerOne} onChange={e => setInputPartnerOne(e.target.value)} placeholder="First Partner" />
-        <input value={inputPartnerTwo} onChange={e => setInputPartnerTwo(e.target.value)} placeholder="Second Partner" style={{ marginLeft: 3 }} />
-      </div>
-      <div>
-        <button onClick={setWeddingCertificate}>Add new Wedding Certificate</button>
-      </div>
-      {partnerOne !== "" && partnerTwo !== "" ? <p>{partnerOne}<FontAwesomeIcon icon={faPeopleArrowsLeftRight} style={{ marginLeft: 3, marginRight: 3 }} />{partnerTwo}</p> : null}
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <div>
+            <h2>Add your wedding certificat <FontAwesomeIcon icon={faIdCardClip} style={{ marginLeft: 3, marginRight: 3 }} />!</h2>
+          </div>
+          <div>
+            <input value={inputPartnerOne} onChange={e => setInputPartnerOne(e.target.value)} placeholder="First Partner" />
+            <input value={inputPartnerTwo} onChange={e => setInputPartnerTwo(e.target.value)} placeholder="Second Partner" style={{ marginLeft: 3 }} />
+          </div>
+          <div>
+            <button onClick={setWeddingCertificate}>Add new Wedding Certificate</button>
+          </div>
+          {partnerOne !== "" && partnerTwo !== "" ?
+            <div>
+              <p>{partnerOne}<FontAwesomeIcon icon={faPeopleArrowsLeftRight} style={{ marginLeft: 3, marginRight: 3 }} />{partnerTwo}</p>
+              <span>Contract address: {weddingCertificateAddress}</span>
+            </div> : null}
+        </CardContent>
+      </Card>
     </div>
   );
 }
